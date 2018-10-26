fa37706103ad214287fd012009bfaff9fe6ea6c1
Zemmouri Rediane <z.rediane24@gmail.com>
2018-10-26 12:10:56 +0100

    Migrated

---
+++ database/migrations/2018_10_26_021115_create_constraints_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConstraintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('constraints', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('child_id');
+            $table->integer('duree');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('constraints');
+    }
+}
